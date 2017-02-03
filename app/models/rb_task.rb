635db4610e9bc6d2809b5db2f1790fdebea2b700
Andy Helten <andy.helten@goctsi.com>
2017-02-03 11:50:59 -0600

    Add support for redmine_subtasks_inherited_fields to taskboard's new task feature

--- app/models/rb_task.rb
+++ app/models/rb_task.rb
@@ -30,8 +30,51 @@ class RbTask < Issue
     return attribs
   end
 
+  def self.inherit_attrs(params)
+    parent_issue = Issue.find(params[:parent_issue_id])
+    settings = Setting.find_by_name("plugin_redmine_subtasks_inherited_fields") || {}
+
+    if ! settings.available_settings.has_key?("plugin_redmine_subtasks_inherited_fields")
+      return nil
+    end
+
+    settings = settings.value if settings.respond_to? :value
+    if settings == ""
+      return nil
+    end
+
+    attrs = {}
+    attrs['fixed_version_id'] = parent_issue.fixed_version_id if settings[:inherit_fixed_version_id]
+    attrs['category_id'] = parent_issue.category_id if settings[:inherit_category_id]
+    attrs['assigned_to_id'] = parent_issue.assigned_to_id if settings[:inherit_assigned_to_id]
+    attrs['priority_id'] = parent_issue.priority_id if settings[:inherit_priority_id]
+    attrs['start_date'] = parent_issue.start_date if settings[:inherit_start_date]
+    attrs['due_date'] = parent_issue.due_date if settings[:inherit_due_date]
+    attrs['description'] = parent_issue.description if settings[:inherit_description]
+    attrs['is_private'] = parent_issue.is_private if settings[:inherit_is_private]
+    attrs['status_id'] = parent_issue.status_id if settings[:inherit_status_id]
+
+    #inherit custom fields
+    inherit_custom_fields = settings[:inherit_custom_fields] || {}
+    unless inherit_custom_fields.empty?
+      custom_field_values = {}
+      parent_issue.custom_field_values.each do |v|
+        custom_field_values[v.custom_field_id] = v.value if inherit_custom_fields[v.custom_field_id.to_s]
+      end
+
+      attrs['custom_field_values'] = custom_field_values unless custom_field_values.empty?
+    end
+
+    return attrs
+  end
+
   def self.create_with_relationships(params, user_id, project_id, is_impediment = false)
     attribs = rb_safe_attributes(params)
+
+    inherited_attribs = inherit_attrs(params)
+    if inherited_attribs
+      attribs = attribs.merge(inherited_attribs)
+    end
 
     attribs['author_id'] = user_id
     attribs['tracker_id'] = RbTask.tracker
